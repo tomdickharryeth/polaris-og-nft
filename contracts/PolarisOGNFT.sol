e0c10c7fbe5b68c5a674021d8dbcd62a0630157e
tomdickharry <wilderworldswallet@gmail.com>
2022-12-26 21:51:41 +0000

    contract updates - add Ownable

--- contracts/PolarisOGNFT.sol
+++ contracts/PolarisOGNFT.sol
@@ -1,15 +1,18 @@
 pragma solidity ^0.8.15;
 
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721A {
-    constructor() ERC721A("Polaris OG NFT", "TPRTL") {}
 
-    function mint(uint256 quantity) external payable {
+contract PolarisOGNFT is ERC721A, Ownable {
+    constructor() ERC721A("Polaris OG NFT", "POLARISOGNFT") {}
+
+    function mint(uint256 quantity) external payable onlyOwner {
         // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
         _mint(msg.sender, quantity);
     }
-
+/* 
     function _baseURI() override internal view virtual returns (string memory) {
         return "ipfs://Qmd5BtjRkbv6zMvdJ9NUxi8dG562HJTpbM9gr5ato3bejb/";
-    }
+    } */
 }
